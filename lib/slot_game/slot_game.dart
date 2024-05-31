665e7f2b97354bc274b80ad1632f26680390e56c
JakcyHsu <ss096210@gmail.com>
2024-05-31 11:03:54 +0800

    change name

--- lib/slot_game/slot_game.dart
+++ lib/slot_game/slot_game.dart
@@ -8,7 +8,7 @@ class SlotGame extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'Flutter Demo',
+      title: 'Golden Empire',
       theme: ThemeData(
         colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
         useMaterial3: true,
